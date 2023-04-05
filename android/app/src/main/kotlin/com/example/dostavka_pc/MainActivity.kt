1702012c8ce67691bb4cdcfdf84b81468a56ca14
Weq <elmir.shakirov.07@mail.ru>
2023-04-05 21:07:05 +0300

    expiremental

--- android/app/src/main/kotlin/com/example/dostavka_pc/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dostavka_pc/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.dostavka_pc
 
 import io.flutter.embedding.android.FlutterActivity
+import com.yandex.mapkit.MapKitFactory;
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    MapKitFactory.setLocale("YOUR_LOCALE") // Your preferred language. Not required, defaults to system language
+    MapKitFactory.setApiKey("YOUR_API_KEY") // Your generated API key
+    super.configureFlutterEngine(flutterEngine)
 }
